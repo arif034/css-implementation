054969c86cf2592e7316b787203934bea74c9df6
arif034 <45455917+arif034@users.noreply.github.com>
2021-12-26 16:41:04 +0530

    css link css reference link

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -5,5 +5,6 @@ class ChildComponent extends Component {
     return <div className="ChildComponentDiv-1">Hello World</div>;
   }
 }
+//http://web.simmons.edu/~grabiner/comm244/weekthree/css-basic-properties.html
 
 export default ChildComponent;
