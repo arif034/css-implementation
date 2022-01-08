94d41ab374d1085258b98efa16f3193900bd0d61
arif034 <45455917+arif034@users.noreply.github.com>
2022-01-08 19:18:35 +0530

    css drawing circles css drawing circles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import "./styles.css";
 import { Component } from "react";
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <div id="circle">
+          <div id="text">Text in the circle</div>
+        </div>
+      </div>
+    );
   }
 }
 export default App;
