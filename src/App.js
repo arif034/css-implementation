100a165bb74b0c8209b7c11248c92b1a7a30f9a8
arif034 <45455917+arif034@users.noreply.github.com>
2022-01-09 10:45:28 +0530

    removing code for circle

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import "./styles.css";
 import { Component } from "react";
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="circle">
-          <div className="text">Text in the circle</div>
-          <div className="radius"></div>
-        </div>
-      </div>
-    );
+    return <div className="App">Hello World</div>;
   }
 }
 export default App;
