ac1ecb9915fd11af77cfc8c94d78400bb668eb32
arif034 <45455917+arif034@users.noreply.github.com>
2022-01-09 14:35:41 +0530

    minor changes in the code minor changes in the code

--- src/component/Circle.js
+++ src/component/Circle.js
@@ -10,91 +10,92 @@ class Circle extends Component {
     };
   }
 
-  toggleColor = () => {
-    setInterval(() => {
-      switch (this.state.toggle) {
-        case 1: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 2,
-              jsx: <div className="circle-2"></div>
-            };
-          });
-          break;
-        }
-        case 2: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 3,
-              jsx: <div className="circle-3"></div>
-            };
-          });
-          break;
-        }
-        case 3: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 4,
-              jsx: <div className="circle-4"></div>
-            };
-          });
-          break;
-        }
-        case 4: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 5,
-              jsx: <div className="circle-5"></div>
-            };
-          });
-          break;
-        }
-        case 5: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 6,
-              jsx: <div className="circle-6"></div>
-            };
-          });
-          break;
-        }
-        case 6: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 7,
-              jsx: <div className="circle-7"></div>
-            };
-          });
-          break;
-        }
-        case 7: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 1,
-              jsx: <div className="circle-1"></div>
-            };
-          });
-          break;
-        }
-        default: {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              toggle: 1,
-              jsx: <div className="circle-1"></div>
-            };
-          });
-          break;
-        }
+  toggleColoring = () => {
+    switch (this.state.toggle) {
+      case 1: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 2,
+            jsx: <div className="circle-2"></div>
+          };
+        });
+        break;
       }
-    }, 1000);
+      case 2: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 3,
+            jsx: <div className="circle-3"></div>
+          };
+        });
+        break;
+      }
+      case 3: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 4,
+            jsx: <div className="circle-4"></div>
+          };
+        });
+        break;
+      }
+      case 4: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 5,
+            jsx: <div className="circle-5"></div>
+          };
+        });
+        break;
+      }
+      case 5: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 6,
+            jsx: <div className="circle-6"></div>
+          };
+        });
+        break;
+      }
+      case 6: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 7,
+            jsx: <div className="circle-7"></div>
+          };
+        });
+        break;
+      }
+      case 7: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 1,
+            jsx: <div className="circle-1"></div>
+          };
+        });
+        break;
+      }
+      default: {
+        this.setState((prevState) => {
+          return {
+            ...prevState,
+            toggle: 1,
+            jsx: <div className="circle-1"></div>
+          };
+        });
+        break;
+      }
+    }
+  };
+  toggleColor = () => {
+    setInterval(this.toggleColoring, 1000);
   };
 
   onPlay() {
