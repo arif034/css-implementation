f9ca616b11d4dac1fbc39b0a7299c8db02032d73
arif034 <45455917+arif034@users.noreply.github.com>
2022-01-09 13:12:46 +0530

    css-implementation for toggling circle colour every second between 2 states css-implementation for toggling circle colour every second between 2 states

--- src/component/Circle.js
+++ src/component/Circle.js
@@ -1,12 +1,35 @@
 import { Component } from "react";
 
 class Circle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: true,
+      jsx: <div className="circle-1"></div>
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.toggle === true) {
+        this.setState(() => {
+          return {
+            toggle: false,
+            jsx: <div className="circle-2"></div>
+          };
+        });
+      } else {
+        this.setState(() => {
+          return {
+            toggle: true,
+            jsx: <div className="circle-1"></div>
+          };
+        });
+      }
+    }, 1000);
+  }
+
   render() {
-    return (
-      <div>
-        <div className="circle"></div>
-      </div>
-    );
+    return <div>{this.state.jsx}</div>;
   }
 }
 export default Circle;
